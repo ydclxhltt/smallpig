a8c8d5e25ad502894b358af32d5641bc8180a128
ydclxhltt <ydclxhltt@users.noreply.github.com>
2014-12-05 18:26:45 +0800

    增加新房界面逻辑

--- SmallPig/SmallPig/FunctionsVC/Home/NewHouseListViewController.m
+++ SmallPig/SmallPig/FunctionsVC/Home/NewHouseListViewController.m
@@ -14,9 +14,41 @@
 
 @implementation NewHouseListViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    //设置title
+    self.title = NEW_HOUSE_TITLE;
+    //设置背景颜色
+    self.view.backgroundColor = NEW_HOUSE_BG_COLOR;
+    //添加返回按钮
+    [self addBackItem];
+    //初始化视图
+    [self createUI];
     // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:YES];
+    //设置不可侧滑
+    [self setMainSideCanSwipe:NO];
+}
+
+#pragma mark 添加UI
+- (void)createUI
+{
+    float start_y = 135.0 + NAV_HEIGHT;
+    UIImage *logoImage = [UIImage imageNamed:@"newhouse_logo.png"];
+    UIImageView *logoImageView = [CreateViewTool createImageViewWithFrame:CGRectMake((SCREEN_WIDTH - logoImage.size.width/2)/2, start_y, logoImage.size.width/2, logoImage.size.height/2) placeholderImage:logoImage];
+    [self.view addSubview:logoImageView];
+    
+    start_y +=  logoImageView.frame.size.height + 10;
+    
+    UILabel *tiplabel = [CreateViewTool createLabelWithFrame:CGRectMake(0, start_y, SCREEN_WIDTH, 50) textString:NEW_HOUSE_TIP textColor:NEW_HOUSE_TIP_TEXT_COLOR textFont:NEW_HOUSE_TIP_FONT];
+    tiplabel.numberOfLines = 2.0;
+    tiplabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:tiplabel];
 }
 
 - (void)didReceiveMemoryWarning {
