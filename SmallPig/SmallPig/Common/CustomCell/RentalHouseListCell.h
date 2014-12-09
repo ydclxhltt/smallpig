0157475bc3d7a7d6af0e0a98369e624f57c65e18
ydclxhltt <ydclxhltt@users.noreply.github.com>
2014-12-09 10:26:16 +0800

    增加注释

--- SmallPig/SmallPig/Common/CustomCell/RentalHouseListCell.h
+++ SmallPig/SmallPig/Common/CustomCell/RentalHouseListCell.h
@@ -9,6 +9,18 @@
 #import <UIKit/UIKit.h>
 @interface RentalHouseListCell : UITableViewCell
 
+/*
+ *  设置图片地址和各个字段的值
+ *
+ *  @pram imageUrl  图片地址
+ *  @pram title     房源描述
+ *  @pram local     房源地区
+ *  @pram park      小区名称
+ *  @pram time      更新时间
+ *  @pram type      房源类型
+ *  @pram size      房源面积
+ *  @pram price     房间价格
+ */
 - (void)setCellImageWithUrl:(NSString *)imageUrl titleText:(NSString *)title localText:(NSString *)local parkText:(NSString *)park timeText:(NSString *)time typeText:(NSString *)type sizeText:(NSString *)size priceText:(NSString *)price;
 
 @end
