af8ac8933b89afde98e1779e23d52788dcf70297
ydclxhltt <ydclxhltt@users.noreply.github.com>
2015-01-29 18:22:03 +0800

    增加地图管理

--- SmallPig/SmallPig/AppDelegate.m
+++ SmallPig/SmallPig/AppDelegate.m
@@ -12,11 +12,13 @@
 #import "WeChatMainViewController.h"
 #import "MineViewController.h"
 #import "AFNetworking.h"
+#import "BMapKit.h"
 
-@interface AppDelegate()
+@interface AppDelegate()<BMKGeneralDelegate>
 {
-    
+    BMKMapManager *mapManager;
 }
+@property (nonatomic, strong) NSString *tokenString;
 @end
 
 
@@ -44,6 +46,23 @@
     //修改架构后屏蔽
     //[[UITabBarItem appearance] setTitleTextAttributes:@{NSFontAttributeName : [UIFont systemFontOfSize:12],NSForegroundColorAttributeName : RGB(190.0, 190.0, 190.0)} forState:UIControlStateNormal];
     //[[UITabBarItem appearance] setTitleTextAttributes:@{NSFontAttributeName : [UIFont systemFontOfSize:12],NSForegroundColorAttributeName : APP_MAIN_COLOR} forState:UIControlStateSelected];
+    
+    
+    //注册远程通知
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)])
+    {
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge|UIUserNotificationTypeSound|UIUserNotificationTypeAlert categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    }
+    else
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeBadge)];
+    
+    //注册百度地图
+    mapManager = [[BMKMapManager alloc] init];
+    [mapManager start:BAIDU_MAP_KEY generalDelegate:self];
+    
+    //初始化数据
+    self.tokenString = @"";
     
     
     //主界面
@@ -113,21 +132,75 @@
 }
 */
 
+#pragma mark 百度SDK启动地图认证Delegate
+- (void)onGetNetworkState:(int)iError
+{
+    if (0 == iError)
+    {
+        NSLog(@"联网成功");
+    }
+    else
+    {
+        NSLog(@"onGetNetworkState %d",iError);
+    }
+    
+}
+
+- (void)onGetPermissionState:(int)iError
+{
+    if (0 == iError)
+    {
+        NSLog(@"授权成功");
+    }
+    else
+    {
+        NSLog(@"onGetPermissionState %d",iError);
+    }
+}
+
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    
+    self.tokenString = [[[[NSString stringWithFormat:@"%@",deviceToken]stringByReplacingOccurrencesOfString:@"<"withString:@""]
+                         stringByReplacingOccurrencesOfString:@">" withString:@""]stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSLog(@"deviceToken: %@====tokenString=====%@", deviceToken,self.tokenString.description);
+}
+
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    application.applicationIconBadgeNumber = 0;
+    /*这里需要处理推送来的消息*/
+    //NSDictionary *auserInfo = [userInfo objectForKey:@"aps"];
+    //NSLog(@"userInfo====%@",auserInfo);
+}
+
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"error====%@",error);
+}
+
+
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [BMKMapView willBackGround];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [BMKMapView didForeGround];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
@@ -137,7 +210,9 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
+    [mapManager stop];
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
 
 @end
